2677965a7f56701968156f35d0645f5d7701b1f1
Anagha003 <101781625+Anagha003@users.noreply.github.com>
2023-11-08 11:54:32 +0530

    Created file3.sh

---
+++ file3.sh
@@ -0,0 +1,11 @@
+#!\bin\bash
+echo "enter num"
+read a b c
+if [ $a -gt $b ] && [ $a -gt $c ]
+then 
+echo "$a is greater"
+elif [ $b -gt $a ] && [ $b -gt $c ]
+then
+echo "$b is greater"
+else
+echo "$c is greater"
